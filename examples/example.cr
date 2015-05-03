35fa226b1152846f5ec4ac6451aeafcd5485c649
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2015-05-03 23:07:44 +0530

    Added REST example

---
+++ examples/example.cr
@@ -0,0 +1,31 @@
+require "./src/moonshine"
+include Moonshine
+include Moonshine::Shortcuts
+
+
+viewcount = 0
+
+app = Moonshine::App.new
+app.define do
+
+	get "/", do |request|
+		viewcount += 1
+		html = "
+			<html>
+				<body>
+					<h1>It worked!</h1>
+					<hr>
+					This page has been visited #{viewcount} times.
+				</body>
+			</html>
+		"
+		ok(html)
+	end
+
+	get "/api", do |request|
+		res = ok("{ name : 'moonshine'}")
+		res.headers["Content-type"] = "text/json"
+		res
+	end
+end
+app.run()
